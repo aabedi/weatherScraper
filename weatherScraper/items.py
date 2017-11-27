11e0cd9ca79e3e1e74100bf74b89fbce9fdb9ec7
Albert Abedi <aabedi3@gatech.edu>
2017-11-27 18:42:38 -0500

    weather spider succesfull in scraping for hardcoded location and date. Next need to implement user-entered parameters from CLI, and work on JSON output.

--- weatherScraper/items.py
+++ weatherScraper/items.py
@@ -12,3 +12,6 @@ class WeatherscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class TempData(scrapy.Item):
+    temperature = scrapy.Field()
--- weatherScraper/spiders/weatherbot.py
+++ weatherScraper/spiders/weatherbot.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
+from weatherScraper.items import TempData
 import scrapy
-
 
 class WeatherbotSpider(scrapy.Spider):
     name = 'weatherbot'
-    allowed_domains = ['www.wunderground.com/history']
+    allowed_domains = ['www.wunderground.com']
     start_urls = ['http://www.wunderground.com/history/']
 
-    # def __init__(self, code='', month='', day='', year='',*args, **kwargs): # this will allow spider arguments 
+    # def __init__(self, code='', month='', day='', year='',*args, **kwargs): # this will allow spider arguments
     #     super(WeatherbotSpider, self).__init__(*args, **kwargs)
     #     self.domain = domain
 
@@ -15,11 +15,12 @@ class WeatherbotSpider(scrapy.Spider):
         return scrapy.FormRequest.from_response(
             response,
             formnumber=1, # formnumber set to 1 because location and date are the second form on history page
-            formdata={'code': 'hnd', 'month': 'November', 'day': '7', 'year': '1994'},
+            formdata = {'code': 'hnd', 'month': '11', 'day': '7', 'year': '1994'},
             # formdata={'code': code, 'month': month, 'day': day, 'year': year},
-            clickdata = { "type": "Submit" },
-            callback=self.after_post
+            # clickdata = { "type": "submit" },
+            callback = self.after_post
         )
+        # return request
 
     def after_post(self, response):
         # check input successful before moving on
@@ -27,5 +28,11 @@ class WeatherbotSpider(scrapy.Spider):
             self.logger.error("Location not valid")
             return
         # only want first 8 of these numbers. use loop
-        return response.css(".wx-value , tr:nth-child(2) .indent+ td").extract()
+        temperatures = TempData()
+        temperatures['temperature'] = []
+        for temp in response.css(".wx-value , tr:nth-child(2) .indent+ td"):
+            temperatures['temperature'].append(temp.css('::text').extract())
+        yield temperatures
+
+        # return response.css(".wx-value , tr:nth-child(2) .indent+ td").extract()
 
