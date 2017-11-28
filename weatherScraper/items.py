1a1dea00cda6173c5dacd4c8d84e51fc1e570866
Albert Abedi <aabedi3@gatech.edu>
2017-11-27 20:36:02 -0500

    Spider now supports arguments inputted in cli, and correctly scrapes information

--- weatherScraper/items.py
+++ weatherScraper/items.py
@@ -15,3 +15,9 @@ class WeatherscraperItem(scrapy.Item):
 
 class TempData(scrapy.Item):
     temperature = scrapy.Field()
+
+class InputData(scrapy.Item):
+    code = scrapy.Field()   # location. airport or city code
+    month = scrapy.Field()  # month, a number b/w 1 and 12 inclusive
+    day = scrapy.Field()    # day, a number b/w 1 and 31 inclusive (february ends at 29?)
+    year = scrapy.Field()   # year, a 4 digit number (how far back is valid?)
--- weatherScraper/spiders/weatherbot.py
+++ weatherScraper/spiders/weatherbot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from weatherScraper.items import TempData
+from weatherScraper.items import InputData
 import scrapy
 
 class WeatherbotSpider(scrapy.Spider):
@@ -7,17 +8,26 @@ class WeatherbotSpider(scrapy.Spider):
     allowed_domains = ['www.wunderground.com']
     start_urls = ['http://www.wunderground.com/history/']
 
-    # def __init__(self, code='', month='', day='', year='',*args, **kwargs): # this will allow spider arguments
-    #     super(WeatherbotSpider, self).__init__(*args, **kwargs)
-    #     self.domain = domain
+
+    def __init__(self, code='', month='', day='', year='', *args, **kwargs): # this will allow spider arguments
+        super(WeatherbotSpider, self).__init__(*args, **kwargs)
+        # self.code = code
+        # self.month = month
+        # self.day = day
+        # self.year = year
+        global user_input
+        user_input = InputData()
+        user_input['code'] = code
+        user_input['month'] = month
+        user_input['day'] = day
+        user_input['year'] = year
 
     def parse(self, response):
         return scrapy.FormRequest.from_response(
             response,
             formnumber=1, # formnumber set to 1 because location and date are the second form on history page
-            formdata = {'code': 'hnd', 'month': '11', 'day': '7', 'year': '1994'},
-            # formdata={'code': code, 'month': month, 'day': day, 'year': year},
-            # clickdata = { "type": "submit" },
+            # formdata = {'code': 'hnd', 'month': '11', 'day': '7', 'year': '2017'},
+            formdata={'code': user_input['code'], 'month': user_input['month'], 'day': user_input['day'], 'year': user_input['year']},
             callback = self.after_post
         )
         # return request
@@ -31,8 +41,17 @@ class WeatherbotSpider(scrapy.Spider):
         temperatures = TempData()
         temperatures['temperature'] = []
         for temp in response.css(".wx-value , tr:nth-child(2) .indent+ td"):
+            # can add label field in item, append here
             temperatures['temperature'].append(temp.css('::text').extract())
+            # temperatures['temperature'].append(temp.extract())
         yield temperatures
 
         # return response.css(".wx-value , tr:nth-child(2) .indent+ td").extract()
+
+        # F or C? might want to parse unit in case it swiches
+
+        # scrapy runspider weatherbot.py -a code=hnd -a month=11 -a day=7 -a year=2017
+
+
+
 
